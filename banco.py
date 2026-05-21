06da00515416d3b4b8fbf462a75070e60cbf8671
Luis Cirino <37421406+luiscirino@users.noreply.github.com>
2026-05-21 15:58:45 -0300

    ATransferido o código do arquivo view.py para o arquivo banco.py por confusão de diretório

---
+++ banco.py
@@ -0,0 +1,5 @@
+# Importando sqlite
+import sqlite3 as lite
+
+# Criando conexão
+con = lite.connect('dados.db')
--- view.py
+++ view.py
@@ -1,7 +1,3 @@
-# Importando sqlite
-import sqlite3 as lite
 
-# Criando conexão
-con = lite.connect('dados.db')
 
 
